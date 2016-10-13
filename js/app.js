e641ac9c7206cb39be2472c52db059dcc8388b52
brmi <desireelenart@gmail.com>
2016-10-12 19:44:14 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$( document ).ready(function() {
+    $('.nav-open').on("click", function(e) {
+	  e.preventDefault();
+	  
+	  $(this).toggleClass('nav-close');
+	});
+});
+	
--- js/soundcloud_script.js
+++
@@ -1,8 +0,0 @@
-SC.initialize({
-  client_id: 'YOUR_CLIENT_ID'
-});
-
-var track_url = 'http://soundcloud.com/forss/flickermood';
-SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-  console.log('oEmbed response: ', oEmbed);
-});
